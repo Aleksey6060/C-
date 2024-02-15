543c083725552ed0e8cd38095873e57908d162e4
Aleksey6060 <145029148+Aleksey6060@users.noreply.github.com>
2024-02-15 10:40:55 +0300

    Add files via upload

---
+++ prac2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main() {
+    const int rows = 3;
+    const int cols = 5;
+    float matrix[rows][cols];
+
+    std::cout << "Enter the elements of the matrix:\n";
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    float row_averages[rows];
+
+    for (int i = 0; i < rows; ++i) {
+        float sum = 0;
+        for (int j = 0; j < cols; ++j) {
+            sum += matrix[i][j];
+        }
+        row_averages[i] = sum / cols;
+    }
+
+    std::cout << "Row averages:\n";
+
+    for (int i = 0; i < rows; ++i) {
+        std::cout << row_averages[i] << '\n';
+    }
+
+    return 0;
+}
